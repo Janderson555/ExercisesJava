664d032f31247e0e7432dd63ce513406b4eca936
Janderson Soares <jandinsm@gmail.com>
2021-04-27 20:34:23 -0300

    Refazendo diretorio

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/introducao-a-javascript-e-logica-de-programacao_3/1.exercise_bloco_4.js
@@ -0,0 +1,14 @@
+let numero = 3;
+
+let quadrado = "";
+
+for(let linha = 0; linha < numero; linha ++){
+        let linhaQ = "";
+    for(let coluna = 0; coluna < numero; coluna ++){
+        quadrado = quadrado + "*";
+    };
+   
+};
+
+
+console.log(quadrad);
